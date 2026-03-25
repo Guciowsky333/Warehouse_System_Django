39f5532732539c50b61f5dfe6adaadbff953b7ef
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-03-25 10:26:35 +0100

    Adding CustomUser model and JWT endpoints to obtain tokens

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('full_name','role')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Role", {"fields": ("role",)}),
+        ("first_name", {"fields": ("first_name",)}),
+        ("last_name", {"fields": ("last_name",)}),
+    )
+
--- users/models.py
+++ users/models.py
@@ -4,6 +4,13 @@ from django.db import models
 # Create your models here.
 
 class CustomUser(AbstractUser):
+
+    # each employee will have a role at the warehouse that will allow him to do certain things
+
+    # warehouseman - can release, check and change localization of components
+    # foreman - can additionally create order lists
+    # manager - can additionally clean whole warehouse and create new users
+
     ROLES = {
         "warehouseman":"warehouseman",
         "foreman":"foreman",
@@ -13,7 +20,11 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def full_name(self):
+        """Returns the full name of the employee"""
+        return f"{self.first_name} {self.last_name}"
+
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.full_name()
 
