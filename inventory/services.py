8e1c5cbf3d425cc9c336ad3e59eea13132e76c18
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-03-28 00:00:06 +0100

    Correct generate_unique_code function

--- inventory/services.py
+++ inventory/services.py
@@ -1,15 +1,18 @@
 import secrets
 import string
+
 def generate_unique_code():
     """This function generates a unique code
     with 15 numbers for each component"""
 
     from inventory.models import Component
+    from inventory.models import ReleasedComponent
+
     while True:
         unique_code = ''.join(secrets.choice(string.digits) for _ in range(15))
 
-        # checking if component with the same unique code already exist
-        if not Component.objects.filter(unique_code=unique_code).exists():
+        # checking if component model or ReleasedComponent model with the same unique code already exist
+        if not Component.objects.filter(unique_code=unique_code).exists() and not ReleasedComponent.objects.filter(unique_code=unique_code).exists():
             return unique_code
 
 
