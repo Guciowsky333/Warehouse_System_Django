b91942f7eedf920715f26eea5279372ddafe093c
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-04-01 22:56:20 +0200

    Adding model ComponentHistory

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from history.models import ComponentHistory
+admin.site.register(ComponentHistory)
 
 # Register your models here.
--- history/models.py
+++ history/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+class ComponentHistory(models.Model):
+    ACTIONS ={
+        "change_location":"change_location",
+        "component_release":"component_release",
+        "component_undo":"component_undo",
+    }
+
+    action = models.CharField(choices=ACTIONS, max_length=17)
+
+    code = models.CharField(max_length=10)
+    unique_code = models.CharField(max_length=15)
+    quantity = models.IntegerField()
+    weight = models.FloatField()
+
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    full_name = models.CharField(max_length=100)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    previous_location = models.CharField(max_length=100)
+    current_location = models.CharField(max_length=100)
+
